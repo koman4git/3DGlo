9975d46f4e9246689467bca2718c7486af60c1c3
Andrey Evtushenko <koman4git@gmail.com>
2022-04-02 16:51:02 +0300

    реализация  функции toggleMenu()

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,18 +1,24 @@
 const menu = () => {
-    
-    const menuBtn = document.querySelector('.menu')
     const menu = document.querySelector('menu')
-    const closeBtn = menu.querySelector('.close-btn')
-    const menuItems = menu.querySelectorAll('ul>li>a')
-
+    
     const handleMenu = () => {
         menu.classList.toggle('active-menu')
     }
-    
+    const toggleMenu = (e) => {
+        if (menu.classList.contains('active-menu')){
+            if (!e.target.closest('.menu')) {
+                handleMenu()
+            } else if (e.target.closest('.menu') && e.target.closest('ul > li > a')) {
+                handleMenu()
+            } else if (e.target.classList.contains('close-btn')) {
+                e.preventDefault()
+                handleMenu()
+            }
+        } else if (e.target.closest('.menu')) {
+            handleMenu()
+        }
+    }
+    document.body.addEventListener('click', toggleMenu)
 
-    menuBtn.addEventListener('click', handleMenu)
-    closeBtn.addEventListener('click', handleMenu)
-
-    menuItems.forEach(menuItem => menuItem.addEventListener('click', handleMenu))
 }
 export default menu;
