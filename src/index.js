564e78d1969a79f77742b915178bd606ce191f0a
Andrey Evtushenko <koman4git@gmail.com>
2022-04-02 16:24:41 +0300

    табы и модальное окно по уроку 22

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import menu from './modules/menu';
 import modal from './modules/modal';
 import calculator from './modules/calculator';
 import validation from "./modules/validation";
+import tabs from "./modules/tabs";
 
 timer('25 april 2022');
 menu()
 modal()
 calculator()
 validation();
+tabs()
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -4,7 +4,6 @@ const modal = () => {
   const modal = document.querySelector(".popup");
   const buttons = document.querySelectorAll(".popup-btn");
   const closeBtn = modal.querySelector(".popup-close");
-  const popupContent = modal.querySelector(".popup-content");
   const screen = window.screen.width;
 
   buttons.forEach((btn) => {
@@ -35,9 +34,11 @@ const modal = () => {
     });
   });
 
-  closeBtn.addEventListener("click", () => {
-    modal.style.display = "none";
-  });
+  modal.addEventListener('click', (e) => {
+    if (!e.target.closest('.popup-content') || e.target.classList.contains('popup-close')) {
+        modal.style.display = "none";
+    };
+  })
 };
 
 export default modal;
