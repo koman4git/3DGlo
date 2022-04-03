76efc5fff1ce1c3cdfa3c39e9b64b10a5023ee0b
Andrey Evtushenko <koman4git@gmail.com>
2022-04-03 11:04:08 +0300

    выполнение урока 23

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import modal from './modules/modal';
 import calculator from './modules/calculator';
 import validation from "./modules/validation";
 import tabs from "./modules/tabs";
+import slider from "./modules/slider";
 
-timer('25 april 2022');
+timer('25 april 2022')
 menu()
 modal()
 calculator()
-validation();
+validation()
 tabs()
+slider()
