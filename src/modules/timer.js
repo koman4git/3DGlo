a83679c3454f4b66273720df45de1ea056c8e49f
Andrey Evtushenko <koman4git@gmail.com>
2022-04-04 21:46:26 +0300

    исправление модуля таймер

--- src/modules/timer.js
+++ src/modules/timer.js
@@ -1,40 +1,50 @@
+"use strict"
+
 const timer = (deadline) => {
-    const timerHours = document.getElementById('timer-hours')
-    const timerMinutes = document.getElementById('timer-minutes')
-    const timerSeconds = document.getElementById('timer-seconds')
-    
-    const getTimeRamaining = () => {
-        let dateStop = new Date(deadline).getTime()
-        let dateNow = new Date().getTime()
-        let timeRemaining = (dateStop - dateNow) / 1000
-        let hours = Math.floor((timeRemaining / 60) / 60)
-        let minutes = Math.floor((timeRemaining / 60) % 60)
-        let seconds = Math.floor(timeRemaining % 60)
+  const timerHours = document.getElementById("timer-hours")
+  const timerMinutes = document.getElementById("timer-minutes")
+  const timerSeconds = document.getElementById("timer-seconds")
 
-        hours = hours < 10 ? "0" + hours : hours;
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-        
-        return {timeRemaining, hours, minutes, seconds}
-    }    
+  let timerId = 0
 
-    const upDateClock = () => {
-        let getTime = getTimeRamaining()
-        
-        timerHours.textContent = getTime.hours
-        timerMinutes.textContent = getTime.minutes
-        timerSeconds.textContent = getTime.seconds
-        if(getTime.timeRemaining > 0) {
-           setInterval(upDateClock, 1000) 
-        } else {
-            timerHours.textContent = '00'
-            timerMinutes.textContent = '00'
-            timerSeconds.textContent = '00'
-        }
-        
+  const getTimeRemaining = () => {
+    let dateStop = new Date(deadline).getTime()
+    let dateNow = new Date().getTime()
+    let timeRemaining = (dateStop - dateNow) / 1000
+    let hours = Math.floor(timeRemaining / 60 / 60)
+    let minutes = Math.floor((timeRemaining / 60) % 60)
+    let seconds = Math.floor(timeRemaining % 60)
+
+    return { timeRemaining, hours, minutes, seconds }
+  };
+
+  const updateClock = () => {
+    let getTime = getTimeRemaining()
+
+    timerHours.textContent = getTime.hours
+    timerMinutes.textContent = getTime.minutes
+    timerSeconds.textContent = getTime.seconds
+
+    if (timerHours.textContent < 10) {
+      timerHours.textContent = "0" + timerHours.textContent
     }
-    upDateClock()
-    getTimeRamaining()
-    
-}
+    if (timerMinutes.textContent < 10) {
+      timerMinutes.textContent = "0" + timerMinutes.textContent
+    }
+    if (timerSeconds.textContent < 10) {
+      timerSeconds.textContent = "0" + timerSeconds.textContent
+    }
+
+    if (getTime.timeRemaining <= 0) {
+      timerHours.textContent = "00"
+      timerMinutes.textContent = "00"
+      timerSeconds.textContent = "00"
+      clearInterval(timerId)
+    }
+  };
+  updateClock()
+
+  timerId = setInterval(updateClock, 1000)
+};
+
 export default timer
